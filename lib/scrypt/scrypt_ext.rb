7922c7c893b1943a493dd4c02b191a63a9b1f38e
Stephen von Takach <steve@webcontrol.me>
2013-07-08 20:06:13 +1000

    prevent blocking on MRI
    This frees up the GIL before calling the function, allowing ruby to
    process other work in parallel (another rails request for example)

--- lib/scrypt/scrypt_ext.rb
+++ lib/scrypt/scrypt_ext.rb
@@ -7,7 +7,7 @@ module SCrypt
     ffi_lib FFI::Compiler::Loader.find('scrypt_ext')
 
     # Bind the external functions
-    attach_function :sc_calibrate, [:double, :double, :pointer], :int
-    attach_function :sc_crypt, [:string, :string, :pointer, :pointer], :int
+    attach_function :sc_calibrate, [:double, :double, :pointer], :int, :blocking => true
+    attach_function :sc_crypt, [:string, :string, :pointer, :pointer], :int, :blocking => true
   end
 end
